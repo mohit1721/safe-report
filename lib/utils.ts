9765e37783abe231d6b8001f3c97f42ee7bfba1e
mohit1721 <mohitmandal192@gmail.com>
2025-02-13 22:52:01 +0530

    13-02-25 V

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+// import { clsx, type ClassValue } from "clsx"
+// import { twMerge } from "tailwind-merge"
+
+// export function cn(...inputs: ClassValue[]) {
+//   return twMerge(clsx(inputs))
+// }
+
+
+// ***tailwind issue
+import clsx from "clsx";
+import { type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+// import * as twMerge from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(...inputs));
 }
