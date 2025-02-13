63e96d224b692b87f8e654c05a9975604408bb2a
mohit1721 <mohitmandal192@gmail.com>
2025-02-13 20:19:47 +0530

    13-02-25

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -22,21 +22,40 @@
 //   globalThis.prisma = db;
 // }
 
+
+// ******
+
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   // Declare a global variable for the Prisma client
+//   var prisma: PrismaClient | undefined; // This is fine as it is
+// }
+// // let prisma: any;
+// let prisma: PrismaClient | undefined; // Use a more specific type instead of 'any'
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!globalThis.prisma) {
+//     globalThis.prisma = new PrismaClient();
+//   }
+//   prisma = globalThis.prisma;
+// }
+// export default prisma;
+
+
+//*****
+//  */
+
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // Declare a global variable for the Prisma client
-  var prisma: PrismaClient | undefined; // This is fine as it is
-}
-// let prisma: any;
-let prisma: PrismaClient | undefined; // Use a more specific type instead of 'any'
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
+
 export default prisma;
